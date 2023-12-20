07fcf5d98e7f968b82abcc8bedd4f75bfe1cb02f
le402n42\uer <Lynna1011>
2023-12-20 17:05:09 +0800

    使控制區的參數隨著Server端的狀況而改變

--- 1220 L14.R
+++ 1220 L14.R
@@ -21,4 +21,24 @@ Color = "red"          #指定顏色
 #plot(Data, col = Color) #將圖畫到開啟中的pdf file上去
 #dev.off()            #關掉這個pdf file
 
+#第三節：使控制區的參數隨著Server端的狀況而改變(1)
+Data = read.table("Example.txt", header = TRUE)
+head(Data, 10)
+summary(Data) 
+Result = lm(Weight~Height, data = Data) #linear regression test
+Result
+summary(Result)
+plot(Data$Height, Data$Weight, pch = 19)
+abline(Result)
+
+
+Data=read.table("Example.txt", header = TRUE)
+colnames(Data)    # 列出所有的欄位名稱
+colnames(Data)[1] # 列出第一個欄位名稱
+colnames(Data)[4] # 列出第四個欄位名稱(依此類推)
+Result1 = lm(Data[,"Weight"]~Data[,"Height"])
+summary(Result1)
+
+
+
 
